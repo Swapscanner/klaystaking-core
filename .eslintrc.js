aa5970aab624dd17ffa943c9e93f3b6461add402
kay <101034096+swapscanner-kay@users.noreply.github.com>
2023-03-21 15:09:42 +0900

    misc: add typescript parser/plugin to eslint (#1)
    * misc: add typescript parser/plugin to eslint
    
    * chore: remove unused import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   env: {
     es2022: true,
     node: true,
