5932f942cb30750814237e25b305b5e22452f236
swapscanner-ryan <ryan@swapscanner.io>
2023-04-28 17:45:35 +0900

    deps: use solc 0.8.18

--- contracts/cnstakinginterfaces/CNStakingV2Interface.sol
+++ contracts/cnstakinginterfaces/CNStakingV2Interface.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity =0.8.19;
+pragma solidity =0.8.18;
 
 import '../external/klaytn/cnstakingv2/CnStakingV2.sol';
 import './CNStakingV1Interface.sol';
