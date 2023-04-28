f9bbce2db2727846022719157c48efb7e91848d1
swapscanner-ryan <ryan@swapscanner.io>
2023-04-28 17:48:01 +0900

    wip

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import 'solidity-coverage';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.19',
+    version: '0.8.18',
     settings: {
       optimizer: {
         enabled: true,
