5e8b57caa2223ea25fc5150f07ccd0b8fd462f2d
Ryan <96642724+swapscanner-ryan@users.noreply.github.com>
2023-04-28 17:56:38 +0900

    deps: use solc 0.8.18 (#37)
    * deps: use solc 0.8.18
    
    * wip
    
    * wip

--- contracts/ProxyStakedKLAYClaimCheck.sol
+++ contracts/ProxyStakedKLAYClaimCheck.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity =0.8.19;
+pragma solidity =0.8.18;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
